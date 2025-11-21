238544da59bb8f55a205a7911686c0d5ce8692b9
Fritz <almenegab@gmail.com>
2025-11-21 22:36:03 +0800

    Add project files.

---
+++ Lyserra/PlayerAndAttributes/Attributes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+
+namespace Lyserra.PlayerAndAttributes
+{
+    public class Attributes
+    {
+        List<string> petType = new List<string>()
+        {
+            "Dog","Cat"
+        };
+        List<string> dogBreed = new List<string>()
+        {
+            "Tiger Commando", "Aspin", "Corgi", "Chihuahua", "Afghan Hound"
+        };
+    }
+}
